5670fd71a55ee6ec2bf1d1f8945f29ff2e035b29
Huseyin Orhan <huseyinsoftware@gmail.com>
2025-05-22 15:38:58 +0300

    bat start

---
+++ publish.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d %~dp0
+echo Publishing Spacetime project to local server...
+spacetime publish --server local stdboffice
---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d %~dp0
+echo Starting Spacetime...
+spacetime start
+pause
